5839e1f181dc887a74a5449feefbafacda5b1af9
Guilherme Chiconato <guilherme.chiconato@ndrive.com>
2025-07-20 16:28:02 -0300

    added concurrency to fetch matches

--- core/repository/src/main/java/com/codechallenge/repository/matches/MatchesPagingSource.kt
+++ core/repository/src/main/java/com/codechallenge/repository/matches/MatchesPagingSource.kt
@@ -1,25 +1,38 @@
 package com.codechallenge.repository.matches
 
+import MatchesSourceType
+import RunningMatches
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.codechallenge.model.Match
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 
 class MatchesPagingSource(
     private val matchesRepository: MatchesRepository,
-    private val upcomingMaxDate: String
+    private val latestDate: String
 ) : PagingSource<Int, Match>() {
 
-    override fun getRefreshKey(state: PagingState<Int, Match>): Int? =
-        state.anchorPosition
+    companion object {
+        private const val STARTING_PAGE = 1
+    }
+
+    private var currentMatchesType: MatchesSourceType = RunningMatches(matchesRepository)
+
+    override fun getRefreshKey(state: PagingState<Int, Match>): Int? {
+        currentMatchesType = RunningMatches(matchesRepository)
+        return STARTING_PAGE
+    }
 
     override val keyReuseSupported: Boolean = true
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Match> = withContext(Dispatchers.IO) {
         try {
-            val currentPage = if ((params.key ?: 0) > 0) params.key!! else 1
+            val currentPage = params.key ?: STARTING_PAGE
 
+            /* TODO In case to fetch with only one request but problem with different sorting
             val matches = matchesRepository.getMatches(
                 pageSize = params.loadSize,
                 pageNumber = currentPage,
@@ -30,6 +43,46 @@ class MatchesPagingSource(
                 data = matches,
                 prevKey = if (currentPage == 1) null else currentPage - 1,
                 nextKey = if (matches.isEmpty()) null else currentPage + 1
+            )
+            */
+
+            /* TODO: Spec doubt - If we can assume the running matches list size are never bigger than 10
+             *  otherwise it will have a problem with pagination so then the next source request needs to be executed after the first one
+             */
+            val matches = if (currentMatchesType.runConcurrentWithNext) {
+                val matches2 = async(start = CoroutineStart.LAZY) {
+                    currentMatchesType = currentMatchesType.nextSource
+                    currentMatchesType.loadMatches(params.loadSize, currentPage, latestDate)
+                }
+                val matches1 = async(start = CoroutineStart.LAZY) {
+                    val match1CurrentMatchesType = currentMatchesType
+                    matches2.start()
+                    match1CurrentMatchesType.loadMatches(params.loadSize, currentPage, latestDate)
+                }
+
+                matches1.start()
+
+                matches1.await() + matches2.await()
+            } else {
+                currentMatchesType.loadMatches(params.loadSize, currentPage, latestDate)
+            }
+
+            val isEndOfTheList = matches.size < params.loadSize
+
+            val nextKey = when {
+                currentMatchesType.isLastSource && matches.isEmpty() -> null
+                isEndOfTheList -> STARTING_PAGE
+                else -> currentPage + 1
+            }
+
+            if (isEndOfTheList) {
+                currentMatchesType = currentMatchesType.nextSource
+            }
+
+            LoadResult.Page(
+                data = matches,
+                prevKey = if (currentPage == STARTING_PAGE) null else currentPage - 1,
+                nextKey = nextKey
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
--- core/repository/src/main/java/com/codechallenge/repository/matches/MatchesRepository.kt
+++ core/repository/src/main/java/com/codechallenge/repository/matches/MatchesRepository.kt
@@ -1,35 +1,61 @@
 package com.codechallenge.repository.matches
 
-import android.util.Log
 import com.codechallenge.model.Match
 import com.codechallenge.network.match.MatchApi
 import com.codechallenge.network.model.toModel
 
 interface MatchesRepository {
-    suspend fun getMatches(pageSize: Int, pageNumber: Int, upcomingMaxDate: String): List<Match>
+    suspend fun getRunningMatches(pageSize: Int, pageNumber: Int, latestDate: String): List<Match>
+    suspend fun getUpcomingMatches(pageSize: Int, pageNumber: Int, latestDate: String): List<Match>
+    suspend fun getPastMatches(pageSize: Int, pageNumber: Int, latestDate: String): List<Match>
 }
 
 class MatchesRepositoryImpl(
     private val matchApi: MatchApi
 ) : MatchesRepository {
 
-    companion object {
-        private const val STATUS = "running,not_started,finished"
-        private const val SORTING = "-status,-begin_at"
+    enum class MatchApiStatus(val value: String) {
+        RUNNING("running"),
+        NOT_STARTED("not_started"),
+        FINISHED("finished")
     }
 
-    override suspend fun getMatches(pageSize: Int, pageNumber: Int, upcomingMaxDate: String): List<Match> =
+    companion object {
+        private const val EARLIEST_DATE = "1990-01-01"
+        private const val SORTING_ASCENDING = "begin_at"
+        private const val SORTING_DESCENDING = "-begin_at"
+    }
+
+    private suspend fun getMatches(
+        pageSize: Int,
+        pageNumber: Int,
+        status: MatchApiStatus,
+        latestDate: String,
+        sort: String
+    ): List<Match> =
         matchApi.getMatches(
             pageSize = pageSize,
             pageNumber = pageNumber,
-            status = STATUS,
-            dates = "1990-01-01, $upcomingMaxDate",
-            sort = SORTING
+            status = status.value,
+            dateRange = "$EARLIEST_DATE, $latestDate",
+            sort = sort
         )
-            .filter { nMatch ->
-                if (nMatch.teams.size < 2) Log.d("TEST", "teams: $nMatch")
-                nMatch.teams.size >= 2
-            } // TODO Why is some matches without two teams?
+            .filter { nMatch -> nMatch.teams.size >= 2 } // TODO Why is some matches without two teams?
             .map { it.toModel() }
+
+    override suspend fun getRunningMatches(pageSize: Int, pageNumber: Int, latestDate: String): List<Match> =
+        getMatches(pageSize = pageSize, pageNumber = pageNumber, latestDate = latestDate, status = MatchApiStatus.RUNNING, sort = SORTING_DESCENDING)
+
+    override suspend fun getUpcomingMatches(pageSize: Int, pageNumber: Int, latestDate: String): List<Match> =
+        getMatches(
+            pageSize = pageSize,
+            pageNumber = pageNumber,
+            latestDate = latestDate,
+            status = MatchApiStatus.NOT_STARTED,
+            sort = SORTING_ASCENDING
+        )
+
+    override suspend fun getPastMatches(pageSize: Int, pageNumber: Int, latestDate: String): List<Match> =
+        getMatches(pageSize = pageSize, pageNumber = pageNumber, latestDate = latestDate, status = MatchApiStatus.FINISHED, sort = SORTING_DESCENDING)
 
 }
