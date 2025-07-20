01e6c202644d92551dac740a3c99a0db70501fb7
Guilherme Chiconato <guilherme.chiconato@ndrive.com>
2025-07-20 16:33:46 -0300

    created async image with placeholder

--- core/designsystem/src/main/java/com/codechallenge/designsystem/components/CirclePlaceholderAsyncImage.kt
+++ core/designsystem/src/main/java/com/codechallenge/designsystem/components/CirclePlaceholderAsyncImage.kt
@@ -1,26 +1,24 @@
 package com.codechallenge.designsystem.components
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codechallenge.designsystem.theme.CstvAppTheme
 
 @Composable
-fun RectangleAsyncImage(
+fun CirclePlaceholderAsyncImage(
     modifier: Modifier = Modifier,
     contentScale: ContentScale = ContentScale.Fit,
     url: String?
 ) {
-    val shape = RoundedCornerShape(CstvAppTheme.spacing.small)
+    val shape = CircleShape
 
     CstvAsyncImage(
-        modifier = modifier
-            .clip(shape),
+        modifier = modifier,
         shape = shape,
         contentScale = contentScale,
         url = url
@@ -31,7 +29,7 @@ fun RectangleAsyncImage(
 @Composable
 private fun RectangleAsyncImagePreview() {
     CstvAppTheme {
-        RectangleAsyncImage(
+        CirclePlaceholderAsyncImage(
             modifier = Modifier
                 .size(60.dp),
             url = ""
